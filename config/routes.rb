621c7622e835fcc393d2a150c9124a1f3f8ae411
Artem Stepanchenko <astepanchenko1@gmail.com>
2024-11-02 10:54:31 +0200

    added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "calculators/index"
+  get "calculators/new"
+  get "calculators/show"
+  get "calculators/create"
+  get "calculators/edit"
+  get "calculators/update"
+  get "calculators/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
